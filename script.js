c5053faa85b88d395a4be3bcacc951acd40c13ce
Glitch (glitch-hello-website) <none>
2023-09-01 16:31:21 +0000

    🌓🚧 Checkpoint

--- script.js
+++ script.js
@@ -1,34 +1,21 @@
-/*
-  This is your site JavaScript code - you can add interactivity!
-*/
+// Existing JS functionalities, if any
 
-// Print a message in the browser's dev tools console each time the page loads
-// Use your menus or right-click / control-click and choose "Inspect" > "Console"
-console.log("Hello 🌎");
+// Audio Player Controls
+const audioElement = document.getElementById('audioElement');
+const playPauseBtn = document.getElementById('playPauseBtn');
+const playPauseIcon = document.getElementById('playPauseIcon');  // Get the icon element
+const volumeSlider = document.getElementById('volumeSlider');
 
-/* 
-Make the "Click me!" button move when the visitor clicks it:
-- First add the button to the page by following the steps in the TODO 🚧
-*/
-const btn = document.querySelector("button"); // Get the button from the page
-if (btn) { // Detect clicks on the button
-  btn.onclick = function () {
-    // The 'dipped' class in style.css changes the appearance on click
-    btn.classList.toggle("dipped");
-  };
-}
+playPauseBtn.addEventListener('click', () => {
+    if (audioElement.paused) {
+        audioElement.play();
+        playPauseIcon.className = "fas fa-pause";  // Switch to Pause Icon
+    } else {
+        audioElement.pause();
+        playPauseIcon.className = "fas fa-play";  // Switch to Play Icon
+    }
+});
 
-
-// ----- GLITCH STARTER PROJECT HELPER CODE -----
-
-// Open file when the link in the preview is clicked
-let goto = (file, line) => {
-  window.parent.postMessage(
-    { type: "glitch/go-to-line", payload: { filePath: file, line: line } }, "*"
-  );
-};
-// Get the file opening button from its class name
-const filer = document.querySelectorAll(".fileopener");
-filer.forEach((f) => {
-  f.onclick = () => { goto(f.dataset.file, f.dataset.line); };
+volumeSlider.addEventListener('input', () => {
+    audioElement.volume = volumeSlider.value;
 });
